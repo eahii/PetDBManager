c88b714f0eae2d79e09960b4e26de136acd36359
eahii <p86385@edu.sasky.fi>
2024-10-30 18:36:24 +0200

    css fixes

--- obj/Debug/net8.0/lemmikkiAPI-esimerkki.AssemblyInfo.cs
+++ obj/Debug/net8.0/lemmikkiAPI-esimerkki.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lemmikkiAPI-esimerkki")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea0b3abaeb58d248ad12f2dde08ccbedc67d0d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+464a2592cfeaa76ffa10eb905f89d694a6111dc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("lemmikkiAPI-esimerkki")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lemmikkiAPI-esimerkki")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
